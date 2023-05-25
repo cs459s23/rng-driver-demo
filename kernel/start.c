f07bb1674150eaa0bdec2a611bc68f50498f5ebb
Daniel DeFreez <defreez@users.noreply.github.com>
2023-05-25 19:50:57 +0000

    read rng

--- kernel/start.c
+++ kernel/start.c
@@ -1,10 +1,14 @@
-
 // entry.S needs one stack per CPU.
 __attribute__ ((aligned (16))) char stack0[4096];
+
+#define RNG_DEVICE_ADDRESS 0x4200100
 
 // entry.S jumps here in machine mode on stack0.
 void
 start()
 {
-  while (1);
+  while (1) {
+    volatile int x = *((int*) RNG_DEVICE_ADDRESS);
+    (void) x;
+  }
 }
